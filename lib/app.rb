ab6407ebaf33fee51d8678013cf4d2dd2e775e7f
Lars Bilke <lars.bilke@ufz.de>
2015-11-10 15:24:23 +0100

    Converting single cards.

--- lib/app.rb
+++ lib/app.rb
@@ -3,7 +3,9 @@ require 'bundler'
 Bundler.setup
 require 'sinatra'
 require 'slim'
+require_relative 'init'
 require_relative 'importer'
+require_relative 'post'
 
 set :views, settings.root + '/../templates'
 
@@ -19,9 +21,10 @@ get '/generate' do
 end
 
 get '/generate-card' do
-  card = params['card-id']
-  # TODO
-  # importer = Importer.new(File.dirname(__FILE__), issue)
-  # importer.import
-  # slim :newsletter, scope: importer
+  card_id = params['card-id']
+  card = Trello::Card.find(card_id)
+  if card
+    post = Post.new(card)
+    slim :entry, locals: { :post => post }
+  end
 end
